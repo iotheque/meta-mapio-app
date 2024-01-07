23f01acfa01fa475e30d051f59a08fa9aae52803
Pierrick Curt <pierrickcurt@gmail.com>
2024-01-07 15:51:26 +0000

    fix(mapio-init): refactor the mapio-init script
    This script is called only at first boot after flash.
    It resizes the data partition to its maximal size.
    Stop the services that are using the data parition before the resize.
    Reboot MAPIO at the end.

--- recipes-mapio/mapio-init/mapio-init/mapio-init.sh
+++ recipes-mapio/mapio-init/mapio-init/mapio-init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Stop the services that can use the data partition
+systemctl stop docker
+systemctl stop docker.socket
+systemctl stop usr-local-nvme.automount
+systemctl stop usr-local-nvme.mount
+
 # Resize data partiton to maximal size
 umount /dev/mmcblk1p4
 parted -s /dev/mmcblk1 resizepart 4 100%
@@ -8,3 +14,6 @@ mount /dev/mmcblk1p4 /usr/local
 
 # Signal that first boot has been done
 touch /boot/first_boot_done
+
+# Reboot
+reboot
